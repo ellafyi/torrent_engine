037660708eb9d90ca59ab53e721affdd0cda2d23
Ella Gyurkovská <ella@ella.gy>
2026-04-22 16:31:44 +0200

    feat: define message types in BitTorrent protocol

---
+++ Downpour.Protocol/Messages.fs
@@ -0,0 +1,22 @@
+﻿namespace Downpour.Protocol
+
+type PeerMessage =
+    | KeepAlive
+    | Choke
+    | Unchoke
+    | Interested
+    | NotInterested
+    | Have of pieceIndex: int
+    | Bitfield of bits: byte[]
+    | Request of pieceIndex: int * blockOffset: int * blockLength: int
+    | Piece of pieceIndex: int * blockOffset: int * data: byte[]
+    | Cancel of pieceIndex: int * blockOffset: int * blockLength: int
+    | Port of listenPort: uint16
+
+
+type PeerState =
+    { AmChoking: bool
+      AmInterested: bool
+      PeerChoking: bool
+      PeerInterested: bool
+      PeerBitfield: byte[] }
---
+++ Downpour.Protocol/Serializer.fs
@@ -0,0 +1,15 @@
+﻿module Downpour.Protocol.Serializer
+
+let serialize (message: PeerMessage) : byte[] =
+    match message with
+    | KeepAlive -> [||]
+    | Choke -> [||]
+    | Unchoke -> [||]
+    | Interested -> [||]
+    | NotInterested -> [||]
+    | Have v -> [||]
+    | Bitfield bf -> [||]
+    | Request(pieceIndex, blockOffset, blockLength) -> [||]
+    | Piece(pieceIndex, blockOffset, bytes) -> [||]
+    | Cancel(pieceIndex, blockOffset, blockLength) -> [||]
+    | Port listenPort -> [||]
