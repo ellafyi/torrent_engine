232dd8e14f488df6fe29205ec05fe970babc623a
Ella Gyurkovská <ella@ella.gy>
2026-04-23 22:39:43 +0200

    feat: add types for Tracker component

---
+++ Downpour.Tracker/Types.fs
@@ -0,0 +1,35 @@
+﻿namespace Downpour.Tracker
+
+open System.Net
+
+type AnnounceEvent =
+    | None
+    | Started
+    | Completed
+    | Stopped
+
+type AnnounceRequest =
+    { InfoHash: byte[]
+      PeerId: byte[]
+      Port: uint16
+      Uploaded: int64
+      Downloaded: int64
+      Left: int64
+      Event: AnnounceEvent
+      TrackerId: string option }
+
+type PeerInfo = { IP: IPAddress; Port: uint16 }
+
+type AnnounceResponse =
+    { Interval: int
+      MinInterval: int option
+      TrackerId: string option
+      Seeders: int
+      Leechers: int
+      Peers: PeerInfo list }
+
+type TrackerError =
+    | TrackerFailure of reason: string
+    | NetworkError of message: string
+    | ParseError of message: string
+    | Timeout
