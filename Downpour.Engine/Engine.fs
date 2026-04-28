967c1480b9cd57527b40a0e318d9b31a3ba41bfc
Ella Gyurkovská <ella@ella.gy>
2026-04-28 19:32:32 +0200

    feat: add Engine Interface

---
+++ Downpour.Engine/Engine.fs
@@ -0,0 +1,19 @@
+﻿namespace Downpour.Engine.IEngine
+
+open System.Threading.Tasks
+open Downpour.Engine.Types
+
+type Engine =
+    abstract StartAsync: unit -> Task
+    abstract StopAsync: unit -> Task
+
+    abstract AddTorrentAsync: torrentBytes: byte[] * savePath: string -> Task<int>
+    abstract RemoveTorrentAsync: torrentId: int * deleteFiles: bool -> Task
+    abstract PauseTorrentAsync: torrentId: int -> Task
+    abstract ResumeTorrentAsync: torrentId: int -> Task
+
+    abstract UpdateSettingsAsync: EngineSettings -> Task
+
+    abstract GetTorrents: unit -> TorrentProgress list
+
+    abstract Events: System.IObservable<EngineEvent>
